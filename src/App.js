e3bbb350138da813ac465077f4fc63b4eda28c53
tsawaengsri <tsawaengsri@usf.edu>
2022-06-27 22:01:35 -0400

    Deploy my react app to Github Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import profileImage from './img/puffles2.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My React App</h1>
+     <h2>Author: Thapani Sawaengsri</h2>
+     <img src={profileImage} alt="puffles"/>
     </div>
   );
 }
